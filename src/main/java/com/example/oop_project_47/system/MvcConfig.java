b51368984e9891f873c78ef91e01448be9fc2bd5
RajTripathi20 <86649637+RajTripathi20@users.noreply.github.com>
2021-11-29 16:14:04 +0530

    Login and Registration Working

--- src/main/java/com/example/oop_project_47/system/MvcConfig.java
+++ src/main/java/com/example/oop_project_47/system/MvcConfig.java
@@ -1,5 +1,5 @@
 package com.example.oop_project_47.system;
-
+/*
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/forgot_pass").setViewName("/ForgetPass");
+        /*registry.addViewController("/forgot_pass").setViewName("/ForgetPass");
         registry.addViewController("/register").setViewName("SignUppage_1");
         registry.addViewController("/dashboard").setViewName("/DashboardModule2/AdminDashboard/DashboardAdmin");
-        registry.addViewController("/").setViewName("/LandingPage");
-        registry.addViewController("/login").setViewName("/templates/Authenticate.html");
+        registry.addViewController("/").setViewName("/LandingPage");/*
+        registry.addViewController("/login").setViewName("/Authenticate");
         registry.addViewController("/pass_change").setViewName("/SignInModule1/SuccessfulChange");
-        registry.addViewController("/registered").setViewName("/SuccessfulRegistration");
+        registry.addViewController("/registered").setViewName("/SuccessfulRegistration");*//*
     }
 
-}
+}*/
