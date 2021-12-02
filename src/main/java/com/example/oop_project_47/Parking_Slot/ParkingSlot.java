f9e575c24135f2e9e678067c28988fee9b5a8a9c
RajTripathi20 <86649637+RajTripathi20@users.noreply.github.com>
2021-12-02 13:07:34 +0530

    Created all tables

--- src/main/java/com/example/oop_project_47/Parking_Slot/ParkingSlot.java
+++ src/main/java/com/example/oop_project_47/Parking_Slot/ParkingSlot.java
@@ -2,8 +2,16 @@ package com.example.oop_project_47.Parking_Slot;
 
 import com.example.oop_project_47.Model.NamedEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class ParkingSlot extends NamedEntity {
+
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "Slot Type")
+    private Integer slotType;
 }
--- src/main/java/com/example/oop_project_47/Parking_Space/ParkingSpace.java
+++ src/main/java/com/example/oop_project_47/Parking_Space/ParkingSpace.java
@@ -1,9 +1,36 @@
 package com.example.oop_project_47.Parking_Space;
 
 import com.example.oop_project_47.Model.NamedEntity;
+import com.example.oop_project_47.Worker.Worker;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class ParkingSpace extends NamedEntity {
+
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "Location")
+    private String location;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "Hatchback Slots")
+    private Integer hatchbackSlots;
+
+    @Column(name = "Sedan Slots")
+    private Integer sedanSlots;
+
+    @Column(name = "SUV Slots")
+    private Integer suvSlots;
+
+    @Column(name = "Buffer Slots")
+    private Integer bufferSlots;
+
+    @Column(name = "Number of Workers")
+    private Integer workerCount;
+
 }
